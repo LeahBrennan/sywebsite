b5e42d700b76d58b3f9b0e9f7a8d0b4ee76f2ef4
LeahBrennan <33582779+LeahBrennan@users.noreply.github.com>
2017-11-11 21:07:11 +0000

    skeleton

---
+++ about.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Irish Ladies Rugby</title>
+	
+    <meta charset="utf-8">
+	<style>
+  <?php include 'css/style.css';?>
+	</style>
+</head>
+<h1>Irish Women's Rugby</h1>
+	
+<ul>
+<li><a href="index.php?action=home">Home</a></li>
+<li><a href="index.php?action=about">About</a></li>
+<li><a href ="index.php?action=sitemap">Sitemap </a></li>
+<li><a href = "index.php?action=people">People</a></li>
+<li><a href = "index.php?action=shopping">Shopping</a></li>
+<li><a href = "index.php?action=stafflogin">Staff Login</a></li>
+<li><a href = "index.php?action=adminlogin">Admin Login</a></li>
+<li><a href = "index.php?action=signup">Sign-up</a></li>
+</ul>
+
+<body>
+
+<p>
+   About
+</p>
+<hr>
+
+<p>
+    
+</p>
+
+<footer>
+</footer>
+</body>
+</html>
---
+++ error.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>MGW - error page</title>
+    <meta charset="utf-8">
+</head>
+
+<body>
+
+<h1>error</h1>
+<p>
+    Sorry, the action requested cannot be found
+</p>
+<hr>
+
+<p>
+    <a href="index.php?action=index">(to home page)</a>
+</p>
+<p>
+    <a href="index.php?action=about">(to about page)</a>
+</p>
+
+<footer>
+
+</footer>
+</body>
+</html>
