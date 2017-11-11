907c929427f9b92ce0f62245665136d5825078e7
LeahBrennan <33582779+LeahBrennan@users.noreply.github.com>
2017-11-11 21:23:41 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -1,39 +1,40 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+use Itb\MainController;
 
-<!doctype html>
-<html lang="en">
-<head>
-    <title>Irish Women's Rugby</title>
-    <meta charset="utf-8">
-	
- <style>
-<?php include 'css/style.css';?>
- </style>
-	
-</head>
-<h1>Irish Women's Rugby</h1>
-	
-<ul>
-<li><a href="index.php?action=home">Home</a></li>
-<li><a href="index.php?action=about">About</a></li>
-<li><a href ="index.php?action=sitemap">Sitemap </a></li>
-<li><a href = "index.php?action=people">People</a></li>
-<li><a href = "index.php?action=shopping">Shopping</a></li>
-<li><a href = "index.php?action=stafflogin">Staff Login</a></li>
-<li><a href = "index.php?action=adminlogin">Admin Login</a></li>
-<li><a href = "index.php?action=signup">Sign-up</a></li>
-</ul>
-<body>
+$mainController = new MainController();
 
-<p>
-    Hello and Welcome to the site
-</p>
-<hr>
 
-<p>
-    
-</p>
+$action = filter_input(INPUT_GET, 'action');
 
-<footer>
-</footer>
-</body>
-</html>
+
+
+switch($action){
+    case 'about':
+        $mainController->aboutAction();
+        break;
+    case 'index':
+    case 'home':
+        $mainController->indexAction();
+        break;
+	case 'sitemap':
+	     $mainController ->sitemapAction();
+	     break;
+    case 'people':
+	     $mainController ->peopleAction();
+		 break;
+    case 'shopping':
+	      $mainController ->shoppingAction();
+		  break;
+    case 'stafflogin':
+	      $mainController ->staffloginAction();
+		  break;
+    case 'adminlogin':
+	      $mainController ->adminloginAction();
+		  break;
+    case 'signup':
+	      $mainController ->signupAction();
+		  break;
+    default:
+            $mainController->error404Action();
+}
